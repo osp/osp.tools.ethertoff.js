fcbe7a1830ef0e37a153f536ec2b475b8cc946f4
Eric Schrijver <eric@ericschrijver.nl>
2017-02-01 09:37:46 +0100

    Refactor to remove unused coffeeify code from back end

--- server/store.js
+++ server/store.js
@@ -1,5 +1,4 @@
 var liveDbMongo = require('livedb-mongo');
-var coffeeify = require('coffeeify');
 
 module.exports = store;
 
@@ -11,24 +10,14 @@ function store(derby) {
 
   var store = derby.createStore(opts);
 
-  store.on('bundle', function(browserify) {
-
-    // For the navigation menu, we don’t want to transmit the text content of each document,
-    // just the path cf. https://groups.google.com/d/msg/derbyjs/4DWls2HvWyw/ahZBdlYxNGcJ
-    store.shareClient.backend.addProjection("paths", "documents", "json0", {
-        path: true,
-        mime: true,
-        binary: true
-    });
-
-    browserify.transform({global: true}, coffeeify);
-
-    var pack = browserify.pack;
-    browserify.pack = function(opts) {
-      var detectTransform = opts.globalTransform.shift();
-      opts.globalTransform.push(detectTransform);
-      return pack.apply(this, arguments);
-    };
+  // Add a Database projection when querying all documents, that excludes the `text` field
+  // For the navigation menu, we don’t want to transmit the text content of each document
+  // to the client each time, just the path
+  // cf. https://groups.google.com/d/msg/derbyjs/4DWls2HvWyw/ahZBdlYxNGcJ
+  store.shareClient.backend.addProjection("paths", "documents", "json0", {
+      path: true,
+      mime: true,
+      binary: true
   });
 
   return store;
